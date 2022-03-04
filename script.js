9ba4d70212260daeb85ed360d0671cf518479d4e
GytAndr <andriejunas@gmail.com>
2022-03-04 21:23:10 +0300

    multiple divs change color on mouseover

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const container = document.getElementById("container");
-const gridField= document.createElement("div");
-gridField.className = "cell";
-container.appendChild(gridField);
-
-//function to change color of cell
-function changeColor () {
-    gridField.style.background = "black";
-}
-
-//Event listener for mouse down
-gridField.addEventListener('mousedown', changeColor);
-
-
 //how to create more divs?
 //set wariable for how many divs i want
+let countOfCells = 13*2;
 //loop until [i] = how many divs you want
-//with each loop add (1)new div and (2)attach class, 
-//(3) append to parent element.
+for (let index = 0; index <= countOfCells ; index++) {
+    //with each loop add (1)new div
+    gridField= document.createElement("div");
+    //(2)attach class
+    gridField.className = "cell";
+    //(3) append to parent element
+    container.appendChild(gridField);   
+}
+
+// //Event listener for mouse down and loop (for eachOf) trough query Array
+document.querySelectorAll('.cell').forEach(item => {
+    item.addEventListener('mousemove', e => {
+        item.style.background = "black";
+    })
+});
