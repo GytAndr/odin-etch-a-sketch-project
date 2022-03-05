b9a4d5f4ab7dbd85f1c98644f035e803cfeb1b91
GytAndr <andriejunas@gmail.com>
2022-03-05 09:59:26 +0300

    added reset btn and more cells

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const container = document.getElementById("container");
-//how to create more divs?
+
+
+//************START of button to reset canvas************
+const btn = document.createElement("button");
+//add id to button
+btn.id = "resetBtn";
+//append text to button
+btn.textContent = "Reset";
+//append button to body before #conainer div
+document.body.insertBefore(btn, container);
+//************END of button to reset canvas************
+
+
+//************START of creating canvas of multipe divs************
 //set wariable for how many divs i want
-let countOfCells = 13*2;
+let countOfCells = 1023;
 //loop until [i] = how many divs you want
 for (let index = 0; index <= countOfCells ; index++) {
     //with each loop add (1)new div
@@ -11,10 +24,19 @@ for (let index = 0; index <= countOfCells ; index++) {
     //(3) append to parent element
     container.appendChild(gridField);   
 }
+//************END of creating canvas of multipe divs************
 
-// //Event listener for mouse down and loop (for eachOf) trough query Array
+//************START of event listeners************
+// (1st event listener) - change divs color to black on mousemove
+//Event listener for mouse down and loop (for eachOf) trough query Array
 document.querySelectorAll('.cell').forEach(item => {
     item.addEventListener('mousemove', e => {
         item.style.background = "black";
     })
 });
+// (2nd event listener - reset canvas on button press)
+btn.addEventListener("click", (e) => {
+    document.querySelectorAll(".cell").forEach(item => {
+        item.style.background = "white";
+    })
+})
